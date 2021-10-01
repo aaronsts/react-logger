43bab34e6d0217dfa41a03de595d8684db6393ff
aaron staes <aaron.staes@gmail.com>
2021-10-01 16:23:11 +0200

    added first component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,16 @@
 import "./ExpenseItem.scss";
 
 function ExpenseItem() {
+  const expenseDate = new Date(2021, 2, 28);
+  const expenseDescription = "Vitamines";
+  const expenseAmount = 35;
+
   return (
     <div className="expense-item">
-      <h2>29 September 2021</h2>
+      <h2>{expenseDate.toISOString()}</h2>
       <div>
-        <h2 className="expense-item-description">Vitamines</h2>
-        <h3 className="expense-item-price">35 euro</h3>
+        <h2 className="expense-item-description">{expenseDescription}</h2>
+        <p className="expense-item-price">{expenseAmount} euro</p>
       </div>
     </div>
   );
